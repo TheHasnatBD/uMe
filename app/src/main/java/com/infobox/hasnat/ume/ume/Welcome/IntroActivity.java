913af97bdd1eacd29a5cf7bac2ae67b4fdd026f8
Abdullah Al-Hasnat <abdullahalhasnat@gmail.com>
2018-07-06 18:16:58 +0600

    Splash Screen, Intro Slider Screen Compeleted and Firebase Configaration also finished

---
+++ app/src/main/java/com/infobox/hasnat/ume/ume/Welcome/IntroActivity.java
@@ -0,0 +1,200 @@
+package com.infobox.hasnat.ume.ume.Welcome;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.infobox.hasnat.ume.ume.MainActivity;
+import com.infobox.hasnat.ume.ume.R;
+
+public class IntroActivity extends Activity {
+
+    private ViewPager viewPager;
+    private ViewPagerAdapter viewPagerAdapter;
+    private LinearLayout dotsLayout;
+    private TextView[] dots;
+    private int[] layouts;
+    private Button btnSkip, btnNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //For Full Screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.intro_activity);
+
+        // saving in local cache through Shared Preferences
+        SharedPreferences sharedPreferences = getSharedPreferences("PREFS", MODE_PRIVATE);
+        if(sharedPreferences.getInt("INTRO", 0) == 1){
+            startActivity(new Intent(IntroActivity.this, MainActivity.class));
+            finish();
+        }
+
+
+        // initializations
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
+        btnSkip = (Button) findViewById(R.id.btn_skip);
+        btnNext = (Button) findViewById(R.id.btn_next);
+
+        layouts = new int[]{
+                R.layout.slide1,
+                R.layout.slide2,
+                R.layout.slide3};
+
+        // adding bottom dots
+        addBottomDots(0);
+
+        viewPagerAdapter = new ViewPagerAdapter();
+        viewPager.setAdapter(viewPagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+
+
+    }
+
+    //Click Listener for both buttons
+    public  void btnSkipClick(View v){
+        launchHomeScreen();
+    }
+
+    public  void btnNextClick(View v){
+        // checking for last page
+        /**
+         * if last page home screen will be launched
+         */
+        int current = getItem(1);
+        if (current < layouts.length){
+            /** move to next screen   */
+            viewPager.setCurrentItem(current);
+        } else {
+            launchHomeScreen();
+        }
+    }
+
+    /**
+    Add PageChangeLister for PageAdapter
+     */
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageSelected(int position) {
+            addBottomDots(position);
+
+            /**
+             * changing the next button text 'NEXT' / 'GOT IT'
+             */
+            if (position == layouts.length - 1) {
+                // last page. make button text to GOT IT
+                btnNext.setText(getString(R.string.start));
+                btnSkip.setVisibility(View.GONE);
+            } else {
+                // still pages are left
+                btnNext.setText(getString(R.string.next));
+                btnSkip.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+        }
+
+    }; //Add PageChangeLister for PageAdapter End
+
+
+    private void addBottomDots(int currentPage) {
+        dots = new TextView[layouts.length];
+
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(getResources().getColor(R.color.dot_inactive));
+            dotsLayout.addView(dots[i]);
+        }
+
+        if (dots.length > 0)
+            dots[currentPage].setTextColor(getResources().getColor(R.color.dot_active));
+    }
+
+
+
+    private int getItem(int i) {
+        return viewPager.getCurrentItem() + i;
+    }
+
+    private void launchHomeScreen() {
+
+        /**
+         * Save Intro Screen for First Time with shared
+         */
+        SharedPreferences sharedPreferences = getSharedPreferences("PREFS", MODE_PRIVATE);
+        SharedPreferences.Editor editor;
+        editor = sharedPreferences.edit();
+        editor.putInt("INTRO", 1);
+        editor.apply();
+
+
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+
+    /**
+     *  Create class extending PagerAdapter
+     */
+    public class ViewPagerAdapter extends PagerAdapter {
+        private LayoutInflater layoutInflater;
+
+        public ViewPagerAdapter() {
+
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = layoutInflater.inflate(layouts[position], container, false);
+            container.addView(view);
+
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return layouts.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    } //Create class extending PagerAdapter End
+}
---
+++ app/src/main/java/com/infobox/hasnat/ume/ume/Welcome/WelcomeActivity.java
@@ -0,0 +1,35 @@
+package com.infobox.hasnat.ume.ume.Welcome;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.infobox.hasnat.ume.ume.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        //Splash Time and 1st activity
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000); // 3 secs
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intend = new Intent(getApplicationContext(), IntroActivity.class);
+                    startActivity(intend);
+                    finish();
+                }
+            }
+        };
+        myThread.start();
+
+    }
+}
